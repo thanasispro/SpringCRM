2af66ae5e9ff5eed3770af11eeb9d17e2e3239b8
thanasispro <protopapas.dim.ath@outlook.com>
2017-12-05 20:10:37 +0200

    first commit

---
+++ src/com/thanasispro/testdb/Testdb.java
@@ -0,0 +1,50 @@
+package com.thanasispro.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ * Servlet implementation class Testdb
+ */
+@WebServlet("/Testdb")
+public class Testdb extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = "springstudent";
+		String pass = "springstudent";
+		
+		String jdbcUrl="jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		
+		String driver="com.mysql.jdbc.Driver";
+		
+		try{
+			PrintWriter out=response.getWriter();
+			
+			out.println("Connecting...");
+			
+			Class.forName(driver);
+			
+			Connection conn=DriverManager.getConnection(jdbcUrl,user,pass);
+			
+			out.println("SUCESS"+conn);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			throw new ServletException(e);
+			
+			
+		}
+	}
+
+}
